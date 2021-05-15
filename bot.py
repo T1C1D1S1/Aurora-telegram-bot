e678c9acf4fc5ab0ded67db3bfa59c0c60c819ce
Nimi <bnruskcht@gmail.com>
2021-05-15 23:38:08 +0300

    First full working commit, added start_bot.py

--- bot.py
+++ bot.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python
-import telegram
-from telegram.ext import Updater, Dispatcher, CommandHandler
+from telegram.ext import Updater, CommandHandler
 from red_alert import Alert
 import logging
 from snake import snake
 import threading
-import schedule
+from time import sleep
 
 # Enable logging
 logging.basicConfig(
@@ -14,30 +13,66 @@ logger = logging.getLogger(__name__)
 
 TOKEN = open('bot_token', 'r').read()
 
-# setup the bot
-updater = Updater(token=TOKEN, use_context=True)
-dispatcher = updater.dispatcher
 
+class AuroraBot(object):
+    def __init__(self, token, red_alert_places_filter=None):
+        self.red_alert_places_filter = red_alert_places_filter
+        self.run_red_alert = False
+        self.updater = Updater(token, use_context=True)
+        self.dispatcher = self.updater.dispatcher
 
-def red_alert():
-    alert_obj = Alert()
-    alerts = alert_obj.
+    @staticmethod
+    def _bot_ping(update, context):
+        context.bot.send_message(chat_id=update.effective_message.chat_id, text='Bot is up and running')
 
-def bot_ping(update, context):
-    context.bot.send_message(chat_id=update.effective_chat.id, text='Bot is up and running')
+    @staticmethod
+    def _send_snake(update, context):
+        context.bot.send_message(chat_id=update.effective_message.chat_id, text=snake())
 
-def send_snake(update, context):
-    context.bot.send_message(chat_id=update.effective_chat.id, text=snake())
+    def _start_red_alert(self, update, context):
+        if self.run_red_alert:
+            context.bot.send_message(chat_id=update.effective_message.chat_id, text='Red alerts are already running...')
+            return
+        context.bot.send_message(chat_id=update.effective_message.chat_id, text='Red alert updates have been started')
+        self.run_red_alert = True
+        self.red_alert_thread = threading.Thread(target=self._run_red_alert, args=(update, context))
+        self.red_alert_thread.start()
 
-def run_bot
+    def _stop_red_alert(self, update, context):
+        if self.run_red_alert == False:
+            context.bot.send_message(chat_id=update.effective_message.chat_id, text='Red alerts are already stopped...')
+            return
+        self.run_red_alert = False
+        context.bot.send_message(chat_id=update.effective_message.chat_id, text='Red alert updates have been stopped')
+
+    def _run_red_alert(self, update, context):
+        alerts = Alert()
+        while self.run_red_alert:
+            new_alerts = alerts.check_for_update()
+            if new_alerts:
+                if self.red_alert_places_filter:
+                    new_alerts = filter(lambda x: any(substr in x for substr in self.red_alert_places_filter),
+                                        new_alerts)
+                for new_alert in new_alerts:
+                    context.bot.send_message(chat_id=update.effective_message.chat_id,
+                                             text=alerts.notify(new_alert))
+            sleep(2)
+
+    def run(self):
+        snake_handler = CommandHandler('snake', self._send_snake)
+        red_alert_start_handler = CommandHandler('start', self._start_red_alert)
+        red_alert_stop_handler = CommandHandler('stop', self._stop_red_alert)
+        ping_handler = CommandHandler('ping', self._bot_ping)
+        self.dispatcher.add_handler(snake_handler)
+        self.dispatcher.add_handler(red_alert_start_handler)
+        self.dispatcher.add_handler(red_alert_stop_handler)
+        self.dispatcher.add_handler(ping_handler)
+        self.updater.start_polling()
 
 
 def main():
-    # dispatcher.add_handler('start', )
-    snake_handler = CommandHandler('snake', send_snake)
-    dispatcher.add_handler(snake_handler)
-    # t1 = threading.Thread (target=)
-    updater.start_polling()
+    test_bot = AuroraBot(TOKEN)
+    test_bot.run()
 
 
 if __name__ == '__main__':
--- red_alert.py
+++ red_alert.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python
 from hashlib import md5
 from time import time, sleep
+from typing import List
+
 import requests
 
 HASH_PART_1 = b"@Kobi*Snir-"
@@ -8,23 +10,23 @@ HASH_PART_2 = b"-#iOSLinks!"
 SERVER = 'http://80.179.114.130/api2/stt.php'  # gets latest feed
 
 
-class Alert():
+class Alert(object):
     def __init__(self):
         # self.feed = self._extract_ids(self._get_feed())
         self.feed = self._get_feed()
         self.ids = self._get_ids()
 
     def run_loop(self):
-        print('Alerts is running...')
+        print('Alerts service is running...')
         while True:
             updates = self.check_for_update()
             if updates:
                 for update in updates:
                     updates.remove(update)
-                    self.notify_heb(update)
+                    print(self.notify(update))
             sleep(3)
 
-    def check_for_update(self):
+    def check_for_update(self) -> list[dict]:
         new_feed = self._get_feed()
         updates_to_push = list(filter(lambda x: x['id'] not in self.ids, new_feed))
         if updates_to_push:
@@ -32,20 +34,23 @@ class Alert():
             return updates_to_push
 
     @staticmethod
-    def notify_eng(alert):
-        print(f'new red alert at {alert["message"]} at time {alert["date"]}')
+    def notify(alert: dict) -> str:
+        """
+        Formats out an alert
+        :param alert: An alert dict
+        :return: The alert as a string
+        """
+        return f'{alert["date"]}\n{alert["message"]}'
 
-
-    @staticmethod
-    def notify_heb(alert):
-        print(f'{alert["date"]}\nהתראת צבע אדום ב{alert["message"]}')
-
-
-    def _get_ids(self):
+    def _get_ids(self) -> List[str]:
         return list(map(lambda x: x['id'], self.feed))
 
     @staticmethod
-    def _get_feed():
+    def _get_feed() -> List[dict]:
+        """
+        gets the current red alert feed from the alerts server
+        :return: The list
+        """
         timestamp = int(time() * 10000)
         request_hash = md5(HASH_PART_1 + str(timestamp).encode() + HASH_PART_2).hexdigest()
         result = requests.post(SERVER, data={'method': 'getFeed', 'ts': timestamp, 'hash': request_hash})
